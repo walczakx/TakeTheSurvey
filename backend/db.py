ca5d0761439ec7891da3c914f3d7a6a7122c6f07
Jarek <jaroslaw.kulik@tlen.pl>
2018-07-11 01:37:23 +0200

    new sql queries adding completed survey and answers

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,7 @@
 from flask import redirect, url_for
 from flaskext.mysql import MySQL
 
+
 class database:
     def __init__(self, app, config):
         self.mysql = MySQL()
@@ -31,7 +32,7 @@ class database:
         except:
             return redirect(url_for('msg_page'))
 
-    #works
+    # works
     def user_register(self, username, email, password):
         cursor = self.mysql_connect()
         try:
@@ -43,7 +44,7 @@ class database:
         finally:
             self.mysql_finalize()
 
-    #works
+    # works
     def check_if_username_is_free(self, username):
         cursor = self.mysql_connect()
         try:
@@ -67,7 +68,7 @@ class database:
         finally:
             self.mysql_finalize()
 
-    #works
+    # works
     def get_user_privileges(self, user_id):
         cursor = self.mysql_connect()
         try:
@@ -135,7 +136,7 @@ class database:
         except:
             return redirect(url_for('msg_page'))
 
-    #works
+    # works
     def get_all_question_in_questionbase(self):
         cursor = self.mysql_connect()
         try:
@@ -150,13 +151,13 @@ class database:
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT * FROM questiontags JOIN tags ON questiontags.id_tag = tags.id_tag JOIN questionbase ON questiontags.id_question = questionbase.id_question WHERE tag_description = %s"
-            cursor.execute(cmd, (tag_description, ))
+            cursor.execute(cmd, (tag_description,))
             return cursor.fetchone()
         except:
             # do something
             return
 
-    #works
+    # works
     def get_possible_answers(self, id_question):
         # zwraca mozliwe odpowiedzi dla pytania
         cursor = self.mysql_connect()
@@ -178,7 +179,7 @@ class database:
             # do something
             return redirect(url_for('msg_page'))
 
-    #works
+    # works
     def get_user_password(self, user_id):
         cursor = self.mysql_connect()
         cmd = "SELECT `pass` FROM `users` WHERE id_user = %s"
@@ -201,7 +202,7 @@ class database:
             return redirect(url_for('msg_page'))
 
     def get_survey_respondent(self, completedsurvey_id):
-         # zwraca usera ktory wypelnil ankiete (respondenta)
+        # zwraca usera ktory wypelnil ankiete (respondenta)
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT  `id_user` FROM `completedsurvey` WHERE id_completedsurvey = %d"
@@ -215,11 +216,11 @@ class database:
         return False
 
     def is_user_have_any_questions(self, user_id):
-        #TODO tego nie ma baza pytan jest wspolna, na ta chwile nie ma identyfikacji kto dodal pytanie do bazy
+        # TODO tego nie ma baza pytan jest wspolna, na ta chwile nie ma identyfikacji kto dodal pytanie do bazy
         return False
 
     def is_user_have_any_surveys(self, user_id):
-        #zwraca liczbe ankiet (wzorccow) usera
+        # zwraca liczbe ankiet (wzorccow) usera
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT  count(id_survey) AS NumberOfSurveys FROM `survey` WHERE id_user = %d"
@@ -227,8 +228,9 @@ class database:
             return cursor.fetchone()
         except:
             return False
+
     def is_user_have_any_completed_surveys(self, user_id):
-        #zwraca liczbe ankiet wypelnionych przez usera
+        # zwraca liczbe ankiet wypelnionych przez usera
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT  count(id_completedsurvey) AS NumberOfCompletedSurveys FROM `completedsurvey` WHERE id_user = = %d"
@@ -242,7 +244,7 @@ class database:
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT * FROM `completedanswers` WHERE id_completedsurvey = %d"
-            cursor.execute(cmd,(completedsurvey_id))
+            cursor.execute(cmd, (completedsurvey_id))
             return cursor.fetchone()
         except:
             return redirect(url_for('msg_page'))
@@ -252,15 +254,23 @@ class database:
         cursor = self.mysql_connect()
         try:
             cmd = "SELECT completedsurvey.id_completedsurvey, survey.id_survey, survey.survey_description, completedsurvey.datetime, completedsurvey.id_user FROM `survey` left Join `completedsurvey` ON survey.id_survey = completedsurvey.id_survey WHERE completedsurvey.id_user = %d"
-            cursor.execute(cmd,(user_id))
+            cursor.execute(cmd, (user_id))
             completed_surveys = cursor.fetchall()
             return completed_surveys
         except:
             return redirect(url_for('msg_page'))
 
     def delete_question(self, question_id):
-        #todo admin usuwa pytanie z bazy
-        pass
+        # todo admin usuwa pytanie z bazy
+        cursor = self.mysql_connect()
+        try:
+            cmd = "DELETE FROM `questionbase` WHERE id_question = %d"
+            cursor.execute(cmd, question_id)
+            return cursor.fetchone()
+        except:
+            return redirect(url_for('msg_page'))
+        finally:
+            self.mysql_finalize()
 
     def get_question_from_questionbase_by_id(self, question_id):
         cursor = self.mysql_connect()
@@ -271,6 +281,28 @@ class database:
         except:
             return False
 
-    def  get_correct_answer_for_specific_question(self, question_id):
+    def get_correct_answer_for_specific_question(self, question_id):
         #todo, nie jestem przekonany czy bedzie potrzebne
         pass
+
+    def add_completed_survey(self, id_survey, id_user):
+        # wypelniona ankiete (uzupelnia tabele completedsurvey) add_completed_survey i add_completed_answers_for_completed_survey nalezy wywolac w momencie submit ( w podanej kolejnosci)# add_completed_answers_for_completed_survey nalezy wywolac dla kazdej dodwanej odpowiedzi #add_completed_survey dla ankiety trzeba wykonac tylko raz na samym poczatku po submit
+        cursor = self.mysql_connect()
+        try:
+            cmd = "INSERT INTO `completedsurvey`(`id_survey`, `id_user`) VALUES (%d, %d)"
+            cursor.execute(cmd, (id_survey, id_user))
+            return cursor.fetchone()
+        except:
+            # do something
+            return redirect(url_for('msg_page'))
+
+    def add_completed_answers_for_completed_survey(self, id_surveytemplate, id_question, id_answer):
+        # dodaje odpowiedzi wypelnionej ankiety  (uzupelnia tabele completedanswers) # add_completed_survey i add_completed_answers_for_completed_survey nalezy wywolac w momencie submit ( w podanej kolejnosci) # add_completed_answers_for_completed_survey nalezy wywolac dla kazdej dodwanej odpowiedzi # add_completed_survey dla ankiety trzeba wykonac tylko raz na samym poczatku po submit
+        cursor = self.mysql_connect()
+        try:
+            cmd = "INSERT INTO `completedanswers` (id_surveytemplate, id_question, id_answer, id_completedsurvey) VALUES (%d, %d, %d, (SELECT MAX(id_completedsurvey) from `completedsurvey` ))"
+            cursor.execute(cmd, (id_surveytemplate, id_question, id_answer))
+            return cursor.fetchone()
+        except:
+            # do something
+            return redirect(url_for('msg_page'))
